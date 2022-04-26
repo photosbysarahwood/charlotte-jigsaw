f6675615f5ce43ce9721f1e0238f1df7d4be1ca4
photosbysarahwood <photosbysarahwood@users.noreply.github.com>
2022-04-26 23:55:54 +0000

    deploy: 97099e1deebad38f62f5e0e8608f1a0bb0b631e8

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+var button = document.querySelector(".reference");
+var slider = document.querySelector("#slide-reference");
 
+function sliderToggle() {
+  slider.classList.toggle("active");
+  button.classList.toggle("active");
+}
+
+button.addEventListener("click", sliderToggle);
+
+/* Dragging the components of the Student Activity Center to their targets */
+/* Fetch the elements */
+
+var rectangle = document.querySelector("#sac-body");
+var circle = document.querySelector("#sac-cricle");
+var square = document.querySelector("#sac-square");
+var triangle = document.querySelector("#sac-triangle");
+
+function rectangleDrag() {
+  rectangle.classList.drag("active");
+}
